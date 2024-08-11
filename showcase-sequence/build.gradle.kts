f297571c0695c8b66753ab579e8b456d128e9f31
jocoand <joco.learn@gmail.com>
2024-08-11 14:02:36 +0700

    Remove unused dependency

--- showcase-sequence/build.gradle.kts
+++ showcase-sequence/build.gradle.kts
@@ -44,14 +44,12 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.material3)
 
-    implementation(libs.compose.showcaseview)
+    api(libs.compose.showcaseview)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
