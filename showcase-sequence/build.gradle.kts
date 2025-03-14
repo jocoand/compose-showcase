86db6327c08a18612b7fab47dc6d2e15d5bc9f83
jocoand <joco.learn@gmail.com>
2025-03-14 20:56:49 +0700

    Cleanup libraries

--- showcase-sequence/build.gradle.kts
+++ showcase-sequence/build.gradle.kts
@@ -45,8 +45,6 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.foundation)
 
     api(libs.compose.showcaseview)
