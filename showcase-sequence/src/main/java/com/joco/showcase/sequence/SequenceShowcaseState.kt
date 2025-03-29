216c170742abcff995b415bd6a9e9eeb02aa9d4f
jocoand <joco.learn@gmail.com>
2025-03-29 16:30:00 +0700

    Provide target Rect

--- showcase-sequence/src/main/java/com/joco/showcase/sequence/SequenceShowcaseState.kt
+++ showcase-sequence/src/main/java/com/joco/showcase/sequence/SequenceShowcaseState.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.layout.LayoutCoordinates
 import com.joco.showcaseview.AnimationDuration
 import com.joco.showcaseview.ShowcaseAlignment
@@ -84,5 +85,5 @@ data class SequenceShowcaseTarget(
     val alignment: ShowcaseAlignment,
     val highlight: ShowcaseHighlight,
     val duration: AnimationDuration,
-    val content: @Composable () -> Unit
+    val content: @Composable (Rect) -> Unit
 )
